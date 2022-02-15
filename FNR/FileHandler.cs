cd1dec1569215112d496862d04c9f4c0d4d46b60
en9er <frendjoy55@gmail.com>
2022-02-15 15:32:05 +0500

    Added replace

---
+++ FNR/FileHandler.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FNR
+{
+    internal class FileHandler
+    {
+        public double progress = 0;
+        public string currentFile;
+        public event Action<double> ProgressChanged;
+        public event Action<string> FileChanged;
+        private void OnProgressChanged(double progress)
+        {
+            var eh = ProgressChanged;
+            if (eh != null)
+            {
+                eh(progress);
+            }
+        }
+        private void OnFileChanged(string filename)
+        {
+            var eh = FileChanged;
+            if (eh != null)
+            {
+                eh(filename);
+            }
+        }
+
+        public ObservableCollection<string> getDirectoryFiles(string targetDirectory, SearchType searchType, string[] excludeMasks, string mask = "*")
+        {
+            progress = 0;
+            OnProgressChanged(progress);
+            if (Directory.Exists(targetDirectory))
+            {
+                ObservableCollection<string> files = new ObservableCollection<string>();
+                if (SearchType.NotRecursive == searchType)
+                {
+                    progress = 0;
+                    string[] fileEntries = Directory.GetFiles(targetDirectory, mask);
+                    double step = 100.0 / fileEntries.Length;
+                    foreach (string fileName in fileEntries)
+                    {
+                        Console.WriteLine(fileName);
+                        currentFile = fileName;
+                        OnFileChanged(fileName);
+                        files.Add(fileName);
+                        progress += step;
+                        OnProgressChanged(progress);
+                    }
+
+                    foreach (string emsk in excludeMasks)
+                    {
+                        if (emsk != "" && emsk != null)
+                        {
+                            var filesToExclude = Directory.GetFiles(targetDirectory, emsk);
+                            foreach (var file in filesToExclude)
+                            {
+                                int index = files.IndexOf(file);
+                                if (index != -1)
+                                {
+                                    files.RemoveAt(index);
+                                }
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    string[] fileEntries = Directory.GetFiles(targetDirectory, mask, SearchOption.AllDirectories);
+                    double step = 100.0 / fileEntries.Length;
+                    foreach (string fileName in fileEntries)
+                    {
+                        Console.WriteLine(fileName);
+                        currentFile = fileName;
+                        OnFileChanged(currentFile);
+                        files.Add(fileName);
+                        progress += step;
+                        OnProgressChanged(progress);
+                    }
+                    foreach (string emsk in excludeMasks)
+                    {
+                        if (emsk != "" && emsk != null)
+                        {
+                            var filesToExclude = Directory.GetFiles(targetDirectory, emsk, SearchOption.AllDirectories);
+                            foreach(var file in filesToExclude)
+                            {
+                                int index = files.IndexOf(file);
+                                if (index != -1)
+                                {
+                                    files.RemoveAt(index);
+                                }
+                            }
+                        }
+                    }
+
+                }
+                return files;
+            }
+            else
+            {
+                return new ObservableCollection<string>();
+            }
+        }
+
+        public ObservableCollection<string> findMatches(string findText, ObservableCollection<string> filenames)
+        {
+            progress = 0;
+            OnProgressChanged(progress);
+            ObservableCollection<string> result = new ObservableCollection<string>();
+            double step = 100.0 / filenames.Count;
+            foreach (string filename in filenames)
+            {
+                currentFile = filename;
+                OnFileChanged(currentFile);
+                //var path = filename.Replace("\\", "/");
+                ObservableCollection<string> data = getFileData(filename, "");
+                foreach(string str in data)
+                {
+                    if(str.Contains(findText))
+                    {
+                        result.Add(filename);
+                        progress += step;
+                        OnProgressChanged(progress);
+                        break;
+                    }
+                }
+            }
+            return result;
+        }
+
+        public ObservableCollection<string> getFileData(string filename, string findString)
+        {
+            currentFile = filename;
+            OnFileChanged(currentFile);
+            if (filename == null || filename == "")
+                return new ObservableCollection<string>();
+            progress = 0;
+            OnProgressChanged(progress);
+            var path = filename.Replace("\\", "/");
+            if (findString == "")
+            {
+                ObservableCollection<string> res = new ObservableCollection<string>();
+                using (StreamReader sr = new StreamReader(filename))
+                {
+                    Console.WriteLine(File.GetAttributes(filename));
+                    while (sr.EndOfStream == false)
+                    {
+                        var line = sr.ReadLine();
+                        res.Add(line);
+                        progress = ((double)sr.BaseStream.Position / sr.BaseStream.Length) * 100;
+                        OnProgressChanged(progress);
+                    }
+                }
+                return res;
+            }
+            else
+            {
+                ObservableCollection<string> res = new ObservableCollection<string>();
+                using (StreamReader sr = new StreamReader(filename, System.Text.Encoding.ASCII))
+                {
+                    while (sr.EndOfStream == false)
+                    {
+                        var line = sr.ReadLine();
+                        if (line.Contains(findString))
+                            res.Add(line);
+                        progress = ((double)sr.BaseStream.Position / sr.BaseStream.Length) * 100;
+                        OnProgressChanged(progress);
+                    }
+                }
+                return res;
+            }
+        }
+
+        public void replaceString(string filename, string targetStr, string newString)
+        {
+            currentFile = filename;
+            OnFileChanged(currentFile);
+            if(targetStr != "")
+            {
+                ObservableCollection<string> newFileData = new ObservableCollection<string>();
+                using (StreamReader sr = new StreamReader(filename))
+                {
+                    Console.WriteLine(File.GetAttributes(filename));
+                    while (sr.EndOfStream == false)
+                    {
+                        var line = sr.ReadLine();
+                        if (line.Contains(targetStr))
+                            newFileData.Add(line.Replace(targetStr, newString));
+                        progress = ((double)sr.BaseStream.Position / sr.BaseStream.Length) * 100;
+                        OnProgressChanged(progress);
+                    }
+                }
+                File.Create(filename).Close();
+                File.WriteAllLines(filename, newFileData);
+                
+            }
+        }
+    }
+}
---
+++ FNR/MainViewModel.cs
@@ -0,0 +1,298 @@
+﻿using GalaSoft.MvvmLight.Command;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Input;
+
+namespace FNR
+{
+    enum SearchType { Recursive = 1, NotRecursive}
+    internal class MainViewModel : INotifyPropertyChanged
+    {
+        FileHandler _fileHandler = new FileHandler();
+        public event PropertyChangedEventHandler PropertyChanged;
+        public ICommand IBrowseCommand { get; }
+        public ICommand IFindCommand { get; }
+        public ICommand IReplaceCommand { get; }
+        private string _path = "";
+        private bool _searchTypeRecursive = false;
+        private ObservableCollection<string> _filenames = new ObservableCollection<string>();
+        private string _findText = "";
+        private string _replaceText = "";
+        private string _selectedFilename = "";
+        private double _progress = 0;
+        private ObservableCollection<string> _previewData = new ObservableCollection<string>();
+        private string _currentFile;
+        private string _mask = "";
+        private string[] _excludeMask = new string[10];
+        private bool _useMask = false;
+        private bool _useExcludeMask = true;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public bool SearchTypeRecursive 
+        {
+            get { return _searchTypeRecursive; }
+            set 
+            { 
+                if(_searchTypeRecursive != value)
+                {
+                    _searchTypeRecursive = value;
+                }
+                FilePath = _path;
+                OnPropertyChanged(nameof(SearchTypeRecursive));
+            }
+        }
+
+        public string FindText
+        {
+            get { return _findText; }
+            set
+            {
+                Console.WriteLine(value);
+                _findText = value;
+                OnPropertyChanged(nameof(FindText));
+            }
+        }
+
+        public string ReplaceText
+        {
+            get { return _replaceText; }
+            set
+            {
+                Console.WriteLine(value);
+                _replaceText = value;
+                OnPropertyChanged(nameof(ReplaceText));
+            }
+        }
+
+
+        public int FilesCount
+        {
+            get { return _filenames.Count; }
+        }
+
+        public string FilePath
+        {
+            get { return _path; }
+            set
+            {
+                _path = value;
+                FileNames = _fileHandler.getDirectoryFiles(FilePath, SearchTypeRecursive? SearchType.Recursive: SearchType.NotRecursive, UseExcludeMask? _excludeMask: new string[0]{}, UseMask? Mask:"*");
+                OnPropertyChanged(nameof(FilePath));
+            }
+        }
+
+        public string SelectedFilename
+        {
+            get 
+            {
+                return _selectedFilename; 
+            }
+            set
+            {
+                _selectedFilename = value;
+                Task.Run(() =>
+                {
+                    GetFileData();
+                });
+                OnPropertyChanged(nameof(SelectedFilename));
+                OnPropertyChanged(nameof(CurrentFilename));    
+            }
+        }
+        private void GetFileData()
+        {
+            PreviewFile = _fileHandler.getFileData(SelectedFilename, FindText);
+            OnPropertyChanged(nameof(PreviewFile));
+        }
+        public ObservableCollection<string> PreviewFile
+        {
+            get 
+            { 
+                if(SelectedFilename != "" && SelectedFilename != null)
+                {
+                    return _previewData;
+                }
+                else
+                {
+                    return new ObservableCollection<string>();
+                }
+            }
+            set
+            {
+                if (_previewData == value)
+                    return;
+                _previewData = value;
+                OnPropertyChanged(nameof(PreviewFile));
+            }
+        }
+
+        public ObservableCollection<string> FileNames
+        {
+            get { return _filenames; }
+            set
+            {
+                if(_filenames != value)
+                {
+                    _filenames = value;
+                }
+                OnPropertyChanged(nameof(FileNames));
+                OnPropertyChanged(nameof(FilesCount));
+            }
+        }
+        public MainViewModel()
+        {
+            _excludeMask[0] = "*.exe";
+            _excludeMask[1] = "*.dll";
+            _fileHandler.ProgressChanged += ProgressChanged;
+            _fileHandler.FileChanged += FileChanged;
+            IBrowseCommand = new RelayCommand<string>(x =>
+            {
+                FolderBrowserDialog dialog = new FolderBrowserDialog();
+                dialog.Description = "Choose directory";
+                dialog.ShowNewFolderButton = true;
+                DialogResult result = dialog.ShowDialog();
+                if (result == System.Windows.Forms.DialogResult.OK)
+                {
+                    FilePath = dialog.SelectedPath;
+                }
+            });
+
+            IFindCommand = new RelayCommand<string>(x =>
+            {
+                if(FindText == "")
+                {
+
+                }
+                else
+                {
+                    if (FilePath == "")
+                    {
+                        IBrowseCommand.Execute(null);
+                    }
+                    Task.Run(() =>
+                    {
+                        FileNames = _fileHandler.findMatches(FindText, FileNames);
+                        SelectedFilename = SelectedFilename;
+                    });
+                }
+            });
+
+            IReplaceCommand = new RelayCommand<string>(x =>
+            {
+                foreach(string filename in FileNames)
+                {
+                    _fileHandler.replaceString(filename, FindText, ReplaceText);
+                }
+            });
+        }
+
+        public string CurrentFilename
+        {
+            get { return _currentFile; }
+            set
+            {
+                Console.WriteLine(value);
+                _currentFile = value;
+                OnPropertyChanged(nameof(CurrentFilename));
+            }
+        }
+
+        public string Mask
+        {
+            get
+            {
+                if (!UseMask)
+                {
+                    _mask = "*";
+                }
+                return _mask; 
+            }
+            set
+            {
+                _mask = value;
+                FilePath = _path;
+                OnPropertyChanged(nameof(Mask));
+            }
+        }
+
+        public string ExcludeMask
+        {
+            get
+            {
+                string res = "";
+                foreach(string mask in _excludeMask)
+                {
+                    if(!String.IsNullOrEmpty(mask))
+                        res += mask + ", ";
+                }
+                return res;
+            }
+            set
+            {
+                var masks = new ObservableCollection<string>();
+                _excludeMask = value.Replace(" ", "").Split(',');
+                FilePath = _path;
+                OnPropertyChanged(nameof(ExcludeMask));
+            }
+        }
+
+        private void ProgressChanged(double progress)
+        {
+            Progress = progress;
+        }
+
+        private void FileChanged(string filename)
+        {
+            CurrentFilename = filename;
+        }
+
+        public double Progress
+        {
+            get { return Math.Round(_progress); }
+            set
+            {
+                _progress = value;
+                if(_progress > 100)
+                    _progress = 100;
+                OnPropertyChanged(nameof(Progress));
+                OnPropertyChanged(nameof(ProgressPercent));
+            }
+        }
+        public string ProgressPercent
+        {
+            get { return Progress.ToString() + "%"; }
+        }
+
+        public bool UseMask
+        {
+            get { return _useMask;}
+            set
+            {
+                _useMask = value;
+                FilePath = _path;
+                OnPropertyChanged(nameof(UseMask));
+            }
+        }
+
+        public bool UseExcludeMask
+        {
+            get { return _useExcludeMask; }
+            set
+            {
+                _useExcludeMask = value;
+                FilePath = _path;
+                OnPropertyChanged(nameof(UseExcludeMask));
+                OnPropertyChanged(nameof(ExcludeMask));
+            }
+        }
+    }
+}
